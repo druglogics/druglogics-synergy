48e4e7f68a83c87d8eb4afb843182b46a805a241
JohnZobolas <bblodfon@gmail.com>
2018-01-04 17:54:40 +0100

    Deleted obsolete input files Added new config options, cleaned up toy_ags_config.tab Added comments on several input files for clarification Refactored Launcher.java

--- src/druglogics2/Launcher.java
+++ src/druglogics2/Launcher.java
@@ -5,11 +5,12 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
+import static gitsbe.Util.*;
 import gitsbe.*;
 import drabme.*;
 
 public class Launcher {
-	
+
 	// Gitsbe input
 	public String filenameNetwork = "";
 	public String filenameConfig = "";
@@ -24,19 +25,21 @@ public class Launcher {
 	public String filenameCombinations = "";
 	public String directoryTmpGitsbe;
 	public String directoryTmpDrabme;
-	
+
 	public static void main(String[] args) {
 		Launcher drugLogicsLauncher = new Launcher();
 		drugLogicsLauncher.start(args);
 	}
-	
+
 	public void start(String[] args) {
-		
-		if (environmentalVariableBNETisNULL()) return;
-		if (!setupAndValidateInput(args)) return;
-		
+
+		if (environmentalVariableBNETisNULL())
+			return;
+		if (!setupAndValidateInput(args))
+			return;
+
 		Thread thread = null;
-		
+
 		runGitsbe(thread);
 		runDrabme(thread);
 	}
@@ -57,55 +60,56 @@ public class Launcher {
 			filenameModelOutputs = args[7];
 			filenameCombinations = args[8];
 			directoryTmpDrabme = args[4] + File.separator + args[9];
-		}
-		else if ((args.length == 1) || (args.length == 2)) {
+		} else if ((args.length == 1) || (args.length == 2)) {
 			String directoryInput = args[0];
 			directoryInput = makeDirectoryPathAbsolute(directoryInput);
-			
+
 			// Create project name unless specified
 			if (args.length == 2)
 				projectName = args[1].trim();
 			else
 				projectName = new File(directoryInput).getName();
-			
+
 			DateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
 			Calendar calendarData = Calendar.getInstance();
-			
-			directoryOutput = new File(directoryInput, projectName + "_" + dateFormat.format(calendarData.getTime())).getAbsolutePath();
+
+			directoryOutput = new File(directoryInput, projectName + "_" + dateFormat.format(calendarData.getTime()))
+					.getAbsolutePath();
 			directoryTmpGitsbe = new File(directoryOutput, "gitsbe_tmp").getAbsolutePath();
 			directoryTmpDrabme = new File(directoryOutput, "drabme_tmp").getAbsolutePath();
-			
+
 			directoryOutput = makeDirectoryPathAbsolute(directoryOutput);
 			directoryTmpGitsbe = makeDirectoryPathAbsolute(directoryTmpGitsbe);
 			directoryTmpDrabme = makeDirectoryPathAbsolute(directoryTmpDrabme);
 			directoryModels = new File(directoryOutput, "models").getAbsolutePath();
-			
+
 			System.out.println("Finding input files in directory: " + directoryInput);
 			File[] files = new File(directoryInput).listFiles();
-			
+
 			for (int i = 0; i < files.length; i++) {
 				String filename = files[i].getName();
-				if (!loadFileFromDirectory(filename,directoryInput)) return false;
+				if (!loadFileFromDirectory(filename, directoryInput))
+					return false;
 			}
 			printInputFilesMessage();
-		}
-		else {
+		} else {
 			printNoArgumentsMessage();
 			return false;
 		}
-		
+
 		return true;
 	}
 
-	/** 
-	 * Loads file from input directory and checks for duplicates 
-	 * based on the existence of specified strings in the name of the file.
-	 * Returns true if there is no duplication and file was loaded correctly
+	/**
+	 * Loads file from input directory and checks for duplicates based on the
+	 * existence of specified strings in the name of the file. Returns true if there
+	 * is no duplication and file was loaded correctly
 	 */
 	private boolean loadFileFromDirectory(String filename, String directoryInput) {
 		if (filename.toLowerCase().contains("training")) {
 			if (filenameTrainingData.length() > 0) {
-				System.out.println("Aborting, multiple training data files detected: " + filename + ", " + filenameTrainingData);
+				System.out.println(
+						"Aborting, multiple training data files detected: " + filename + ", " + filenameTrainingData);
 				return false;
 			}
 			filenameTrainingData = new File(directoryInput, filename).getAbsolutePath();
@@ -122,19 +126,23 @@ public class Launcher {
 			}
 			filenameNetwork = new File(directoryInput, filename).getAbsolutePath();
 		} else if (filename.toLowerCase().contains("drugpanel")) {
-			if (filenameDrugs.length() > 0) {System.out.println("Aborting, multiple drug definition files detected: " + filename + ", " + filenameDrugs);
+			if (filenameDrugs.length() > 0) {
+				System.out.println(
+						"Aborting, multiple drug definition files detected: " + filename + ", " + filenameDrugs);
 				return false;
 			}
 			filenameDrugs = new File(directoryInput, filename).getAbsolutePath();
 		} else if (filename.toLowerCase().contains("modeloutputs")) {
 			if (filenameModelOutputs.length() > 0) {
-				System.out.println("Aborting, multiple model output files detected: " + filename + ", " + filenameModelOutputs);
+				System.out.println(
+						"Aborting, multiple model output files detected: " + filename + ", " + filenameModelOutputs);
 				return false;
 			}
 			filenameModelOutputs = new File(directoryInput, filename).getAbsolutePath();
 		} else if (filename.toLowerCase().contains("perturbations")) {
 			if (filenameCombinations.length() > 0) {
-				System.out.println("Aborting, multiple perturbation files detected: " + filename + ", " + filenameCombinations);
+				System.out.println(
+						"Aborting, multiple perturbation files detected: " + filename + ", " + filenameCombinations);
 				return false;
 			}
 			filenameCombinations = new File(directoryInput, filename).getAbsolutePath();
@@ -144,9 +152,9 @@ public class Launcher {
 
 	private void printInputFilesMessage() {
 		System.out.println("\nGitsbe input files:" + "\nConfig:       \t" + filenameConfig + "\nNetwork:      \t"
-				+ filenameNetwork + "\nTraining data:\t" + filenameTrainingData + "\n\nDrabme input files:"
-				+ "\nDrugs:        \t" + filenameDrugs + "\nPerturbations:\t" + filenameCombinations
-				+ "\nModel outputs:\t" + filenameModelOutputs);
+				+ filenameNetwork + "\nTraining data:\t" + filenameTrainingData + "\nModel outputs:\t"
+				+ filenameModelOutputs + "\n\nDrabme input files:" + "\nDrugs:        \t" + filenameDrugs
+				+ "\nPerturbations:\t" + filenameCombinations + "\nModel outputs:\t" + filenameModelOutputs);
 		System.out.println("\nOutput directory: " + directoryOutput + "\n");
 	}
 
@@ -155,7 +163,7 @@ public class Launcher {
 		System.out.println("Usage alternative 1: specify directory with input files:\n"
 				+ "druglogics <input files directory> [project name]\n");
 		System.out.println("Usage alternative 2: specify input files as command line arguments:\n"
-				+ "druglogics <project name> <filename network> <filename config file> <filename steady states file> "
+				+ "druglogics <project name> <filename network> <filename config file> <filename training data> "
 				+ "<output directory> <directory models> <filename drugs> <filename model outputs> "
 				+ "<filename combinations> <directory tmp> ");
 	}
@@ -163,11 +171,10 @@ public class Launcher {
 	private void runDrabme(Thread thread) {
 		int verbosity = 3;
 		int combosize = 2;
-		
+
 		thread = new Thread(new Drabme(verbosity, projectName, directoryModels, filenameDrugs, filenameCombinations,
 				filenameModelOutputs, directoryOutput, directoryTmpDrabme, false, combosize));
 		execute(thread);
-		
 	}
 
 	private void runGitsbe(Thread thread) {
@@ -175,7 +182,7 @@ public class Launcher {
 				filenameConfig, directoryOutput, directoryTmpGitsbe));
 		execute(thread);
 	}
-	
+
 	private void execute(Thread thread) {
 		thread.start();
 		try {
@@ -183,24 +190,6 @@ public class Launcher {
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-	}
-
-	private static boolean environmentalVariableBNETisNULL() {
-		if (System.getenv("BNET_HOME") == null) {
-			System.out.println("Set environment variable BNET_HOME to point to location of BNReduction.sh");
-			System.out.println("BNReduction can be obtained from https://github.com/alanavc/BNReduction");
-			return true;
-		} else return false;
-	}
-	
-	/**
-	 * Makes the directory path absolute (if it is not already)
-	 * @param directory
-	 */
-	private static String makeDirectoryPathAbsolute(String directory) {
-		if ((directory.length() > 0) && !(new File(directory).isAbsolute()))
-			directory = new File(System.getProperty("user.dir"), directory).getAbsolutePath();
-		return directory;
 	}
 
 }
